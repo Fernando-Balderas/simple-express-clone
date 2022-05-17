bf805174b674d0d3ab5ad9f22021d9f8f07604d5
Fernando Balderas Guzmán <fernando.bguzman@gmail.com>
2022-05-17 23:25:19 +0300

    Fix error

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,33 @@
 'use strict'
 
-import HttpServer from './server'
+import { IncomingMessage, ServerResponse } from 'http'
+import HttpServer, { NextHandler } from './server'
 
 const server = new HttpServer(3000)
 
-// server.use((req: ServerRequest, res: ServerResponse, next: NextHandler) => {
-//   res.end('Hello, world!')
-//   next()
-// })
+server.use((req: IncomingMessage, res: ServerResponse, next: NextHandler) => {
+  res.end('Hello, world!')
+  next()
+})
 
-// server.get('/', (req: ServerRequest, res: ServerResponse) => {
+// server.get('/', (req: IncomingMessage, res: ServerResponse) => {
 //   res.status(200).send('Home page')
 // })
 
-// server.post('/api/posts', (req: ServerRequest, res: ServerResponse) => {
+// server.post('/api/posts', (req: IncomingMessage, res: ServerResponse) => {
 //   const post = req.payload
 //   res.status(200).send(post)
 // })
 
 // server.delete(
 //   '/api/posts/:postId',
-//   (req: ServerRequest, res: ServerResponse) => {
+//   (req: IncomingMessage, res: ServerResponse) => {
 //     const postId = req.params.postId
 //     res.status(200).send(`Deleted ${postId}`)
 //   }
 // )
 
-// server.put('/api/posts/:postId', (req: ServerRequest, res: ServerResponse) => {
+// server.put('/api/posts/:postId', (req: IncomingMessage, res: ServerResponse) => {
 //   const postId = req.params.postId
 //   res.status(200).send(`Updated ${postId}`)
 // })
--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,27 @@
-'use strict'
-
 import http, { IncomingMessage, ServerResponse, Server } from 'http'
-
-export type ServerRequest = IncomingMessage
 
 export type NextHandler = (err?: unknown) => void
 
 export type ServerRequestHandler = (
-  req: ServerRequest,
+  req: IncomingMessage,
   res: ServerResponse,
   next: NextHandler
 ) => void
 
+type Route = {
+  path: string
+  method: string
+  stack: any[]
+}
 export default class HttpServer {
   private _server: Server
-  private _stack: any[] = []
+  private _stack: any[]
+  private _routes: Route[]
 
   constructor(private readonly _port: number = 3000) {
-    console.log('into constructor')
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    this._server = http.createServer((req, res) => {
-      res.writeHead(200, { 'Content-Type': 'text/plain' })
-      res.end('It works!')
-    })
+    this._server = http.createServer(this.handler)
+    this._stack = []
+    this._routes = []
   }
 
   // get(path: string, handler: ServerRequestHandler) {}
@@ -34,32 +33,21 @@ export default class HttpServer {
   // put(path: string, handler: ServerRequestHandler) {}
 
   use(middleware: any) {
-    console.log('into use')
-    console.log('middleware ', middleware)
     if (typeof middleware !== 'function') {
       throw new Error('Middleware must be a function!')
     }
-    console.log('stack ', this._stack)
     this._stack.push(middleware)
-    console.log('stack ', this._stack)
   }
 
-  handler(req: ServerRequest, res: ServerResponse) {
-    console.log('into handler')
-    console.log('this _stack ', this._stack)
+  handler = (req: IncomingMessage, res: ServerResponse) => {
     let i = 0
     const errorHandler = (err?: any) => {
-      console.log('into error handler')
       if (err == null) {
         res.writeHead(500, { 'Content-Type': 'text/plain' })
         res.end('Internal Server Error')
       }
     }
     const next = (err?: any) => {
-      console.log('into next')
-      console.log('i ', i)
-      console.log('err ', err)
-      console.log('this _stack ', this._stack)
       if (err != null) return setImmediate(() => errorHandler(err))
       if (i >= this._stack.length) return setImmediate(() => errorHandler())
       const layer = this._stack[i++]
@@ -75,8 +63,6 @@ export default class HttpServer {
   }
 
   listen(): void {
-    console.log('into listen')
-    console.log('this _stack ', this._stack)
     this._server.listen(this._port, () =>
       console.log(`Server running on port ${this._port}`)
     )
